ca6f74cbabb5dd6f30e16278240299dc9c386656
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-03 13:11:29 +0500

    remove eslint error

--- src/components/reusable/Header.tsx
+++ src/components/reusable/Header.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
 import { usePathname } from "next/navigation";
-import Image from "next/image";
 
 const navLink = [
   { name: "Home", path: "/" },
